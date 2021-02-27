5e27b0295e8233cfd891c4b9853de745dbd2c9cb
April Howard <apehow111@gmail.com>
2021-02-26 20:21:40 -0500

    initial commit

---
+++ women-workplace/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 10px 0px 5px 10px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #054d94;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #06643a;
+}
+
+a {
+  color: #06643a;
+  margin: 5px 10px 5 px 10
+}
+</style>
---
+++ women-workplace/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>More Info</h1>
+    <ul>
+       <li><a href="https://galsguide.org/2019/02/25/gals-guide-podcast-hidden-figures/">More Info</a></li>
+       <li><a href="https://galsguide.org/2018/02/09/dorothy-vaughan-your-gal-friday/">Dorothy Vaughan</a></li>
+       <li><a href="https://galsguide.org/2018/02/16/katherine-johnson-your-gal-friday/">Katherine Johnson</a></li>
+       <li><a href="https://galsguide.org/2018/02/23/mary-jackson-your-gal-friday/">Mary Jackson</a></li>
+  </ul>
+  </div>
+</template>
