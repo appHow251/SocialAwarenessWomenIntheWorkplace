ad3b02a3fb09d5cc1c7e1da17db5f67060222abd
April Howard <apehow111@gmail.com>
2021-02-27 01:49:47 -0500

    corrected typo

--- women-workplace/src/views/Experiences.vue
+++ women-workplace/src/views/Experiences.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>What Was Your Experiences</h1>
+        <h1>What Was Your Experience</h1>
         <ul>
             <li>The purpose of this site is to build real solutions</li>
             <li>so please explain what happened while avoiding bashing.</li>
